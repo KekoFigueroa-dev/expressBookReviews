8fa15eee0ff4efa981f1339958d656db6c5c2026
KekoFigueroa-dev <thenaturalistgospel@gmail.com>
2025-04-05 16:51:00 -0400

    added register func

--- router/general.js
+++ router/general.js
@@ -5,9 +5,22 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body;
+    
+    // Check if username and password are provided
+    if (!username || !password) {
+        return res.status(400).json({message: "Username and password are required"});
+    }
+    
+    // Check if username already exists
+    if (users.find(user => user.username === username)) {
+        return res.status(409).json({message: "Username already exists"});
+    }
+    
+    // Add new user
+    users.push({ username, password });
+    return res.status(201).json({message: "User successfully registered"});
 });
 
 public_users.get('/',function (req, res) {
